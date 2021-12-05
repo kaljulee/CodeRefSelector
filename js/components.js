fc2864c17700ef318c4415c8756d5bd883a2dd95
kaljulee <kaljuvlee@gmail.com>
2021-12-05 16:16:05 -0500

    use id-generating functions, add observ data to UI

--- js/components.js
+++ js/components.js
@@ -6,19 +6,23 @@ const SPAN = "span";
 function addRow(doc, position, data) {
   let row = doc.createElement(DIV);
   let container = doc.getElementById("container");
-  row.id = data.id;
+  row.id = generateRowId(data.id);
   if (position % 2 === 0) {
     row.classList.add("even-row");
   } else {
     row.classList.add("odd-row");
   }
+
   // add present control
   let presentControl = doc.createElement(INPUT);
   presentControl.setAttribute("type", "checkbox");
   presentControl.classList.add("present-control");
+
+  // set if control is checked.  this is used later for hiding fields
   presentControl.checked = data.observationId != undefined;
   console.log(data);
   row.appendChild(presentControl);
+
   // add label
   let label = doc.createElement(DIV);
   label.classList.add("row-label");
@@ -26,18 +30,27 @@ function addRow(doc, position, data) {
   labelSpan.innerHTML += data.label;
   label.appendChild(labelSpan);
   row.appendChild(label);
+
   // add measurement input
   let measurement = doc.createElement(INPUT);
+  measurement.id = generateMeasurementId(data.id);
   measurement.classList.add("measurement");
   row.appendChild(measurement);
+
   // add unit input
   let unit = doc.createElement(INPUT);
+  unit.id = generateUnitId(data.id);
   unit.classList.add("unit");
   row.appendChild(unit);
   container.appendChild(row);
 
+  // either hide inputs or set values
   if (!presentControl.checked) {
     measurement.classList.add("hidden");
     unit.classList.add("hidden");
+  }
+  else {
+    measurement.value = data.measurement;
+    unit.value = data.unit;
   }
 }
