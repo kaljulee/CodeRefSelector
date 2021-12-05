815544c491326c06942d7560546aa45853b5c25c
kaljulee <kaljuvlee@gmail.com>
2021-12-05 15:04:37 -0500

    zipperData and getObservationId functions

--- js/functions.js
+++ js/functions.js
@@ -8,3 +8,26 @@ function appendToCodeRefList(doc, data) {
 function buttonClicked(elmnt) {
 elmnt.classList.toggle("clicked-button");
 }
+
+function getObservationId (refId, observs) {
+  let response = undefined;
+  for (let i = 0; i < observs.length; i += 1) {
+    console.log('checking ' + observs[i].code_reference_id + ' vs ' + refId);
+    if (observs[i].code_reference_id === refId)
+    {
+    response = observs[i].id;
+    break;
+    }
+  }
+  return response;
+}
+
+function zipperData(codeRefs, observs) {
+  return codeRefs.reduce((acc, ref) => {
+    const { id, label, class_level_2_id} = ref;
+    const output = {id, label, class_level_2_id};
+    output.observationId = getObservationId(id, observs);
+    acc.push(output);
+    return acc;
+  }, []);
+}
